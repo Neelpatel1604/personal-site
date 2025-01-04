c20f569b7ec6ad4e0cc2a123c348bf69fc4f8cce
Neelpatel1604 <neel_patel2004@outlook.com>
2025-01-04 16:32:09 -0500

    Update position

--- src/data/resume/positions.js
+++ src/data/resume/positions.js
@@ -5,11 +5,9 @@ const positions = [
     link: 'https://www.futurewayinstitute.com/',
     daterange: 'June 2022 - March 2024',
     points: [
-      'Monitored and maintained computer system, ensuring smooth operation.',
-      'Performed data entry tasks with a focus on accuracy and consistency.',
-      'Identified and resolved small hardware and software issues',
-      'Collaborated with the team and performed group task.',
-      'Generated regular reports on system usage and troubleshooting activities.',
+      'Monitored and optimized the performance of all company devices, ensuring efficient operation to support IT courses.',
+      'Managed regular software and system updates across devices, reducing system downtime by 20%.',
+      'Created and maintained documentation for 100% of device configurations and troubleshooting procedures, facilitating seamless operations for faculty and students.',
     ],
   },
 ];
