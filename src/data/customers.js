4a868663c60d0dd1eb3643a93aec0514022aed6b
Neelpatel1604 <neel_patel2004@outlook.com>
2025-02-22 16:29:40 -0500

    Update customer

--- src/data/customers.js
+++ src/data/customers.js
@@ -10,5 +10,16 @@ const data = [
       'Built a product launch website using Squarespace, creating a strong'
       + 'online presence to boost brand recognition by 30% using squarespace',
   },
+  {
+    title: 'Analyze.agency',
+    subtitle: 'Written blog guideline',
+    image: '/images/Customer-2.png',
+    link: 'https://www.upwork.com/freelancers/~011950cb70d4876ab9',
+    startDate: '2024-12-20',
+    endDate: 'Present',
+    desc:
+      'Built 4 ETL work flow from diffrent databases using Streamkap which improved data tranfer by 95%,'
+      + 'test it for production and documented process.',
+  },
 ];
 export default data;
